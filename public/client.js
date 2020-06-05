4047a8c15225fa8bea126dad307af967e85bdca1
Sebastião Lucio <sebastiao_coldplay@hotmail.com>
2020-06-05 10:10:04 -0300

    add compatibility with unity 2019

--- public/client.js
+++ public/client.js
@@ -8,60 +8,142 @@ window.addEventListener('load', function() {
 		
 		var args = Array.prototype.slice.call(arguments, 1);
 		
-		gameInstance.SendMessage("NetworkManager", method, args.join(':'));
+		
+		if(gameInstance!=null)
+		{
+		 gameInstance.SendMessage("NetworkManager", method, args.join(':'));
+		}
+		else if(unityInstance!=null)
+		{
+		  unityInstance.SendMessage("NetworkManager", method, args.join(':'));
+		}
+		
+		
 	};
 
 	socket.on('PONG', function(totalPlayers) {
 				      		
 	  var currentUserAtr = totalPlayers+':'+" ";
-	  gameInstance.SendMessage ('NetworkManager', 'OnPrintPongMsg',currentUserAtr);
+	  if(gameInstance!=null)
+		{
+		 gameInstance.SendMessage ('NetworkManager', 'OnPrintPongMsg',currentUserAtr);
+		}
+		else if(unityInstance!=null)
+		{
+		 unityInstance.SendMessage ('NetworkManager', 'OnPrintPongMsg',currentUserAtr);
+		}
+	  
 	});
 	socket.on('JOIN_SUCCESS', function(id,name,avatar) {
 				      		
 	  var currentUserAtr = id+':'+name+':'+avatar;
-	  gameInstance.SendMessage ('NetworkManager', 'OnJoinGame', currentUserAtr);
+	  if(gameInstance!=null)
+		{
+		 gameInstance.SendMessage ('NetworkManager', 'OnJoinGame', currentUserAtr);
+		}
+		else if(unityInstance!=null)
+		{
+		 unityInstance.SendMessage ('NetworkManager', 'OnJoinGame', currentUserAtr);
+		}
+	  
 	});
 	socket.on('SPAWN_PLAYER', function(id,name,avatar,dx,dz) {
 				      		
 	  var currentUserAtr = id+':'+name+':'+avatar+':'+dx+":"+dz;
-	  gameInstance.SendMessage ('NetworkManager', 'OnSpawnPlayer', currentUserAtr);
+	  if(gameInstance!=null)
+		{
+		  gameInstance.SendMessage ('NetworkManager', 'OnSpawnPlayer', currentUserAtr);
+		}
+		else if(unityInstance!=null)
+		{
+		  unityInstance.SendMessage ('NetworkManager', 'OnSpawnPlayer', currentUserAtr);
+		}
+	 
 	});
 	
 	socket.on('UPDATE_POS_AND_ROT', function(id,dx,dz,rotation) {
 				      		
 	  var currentUserAtr = id+':'+dx+':'+dz+":"+rotation;
-	  gameInstance.SendMessage ('NetworkManager', 'OnUpdatePosAndRot', currentUserAtr);
+	  if(gameInstance!=null)
+		{
+		  gameInstance.SendMessage ('NetworkManager', 'OnUpdatePosAndRot', currentUserAtr);
+		}
+		else if(unityInstance!=null)
+		{
+		 unityInstance.SendMessage ('NetworkManager', 'OnUpdatePosAndRot', currentUserAtr);
+		}
+	 
 	});
 	
 	socket.on('UPDATE_BEST_KILLER', function(name,ranking,kills) {
 				      		
 	  var currentUserAtr = name+":"+ranking+":"+kills;
-	  gameInstance.SendMessage ('NetworkManager', 'OnUpdateBestKillers', currentUserAtr);
+	  if(gameInstance!=null)
+		{
+		  gameInstance.SendMessage ('NetworkManager', 'OnUpdateBestKillers', currentUserAtr);
+		}
+		else if(unityInstance!=null)
+		{
+		  unityInstance.SendMessage ('NetworkManager', 'OnUpdateBestKillers', currentUserAtr);
+		}
+	 
 	});
 
 	  socket.on('UPDATE_SHOOT', function(id) {
 				      		
 	  var currentUserAtr = id+":"+" ";
-	  gameInstance.SendMessage ('NetworkManager', 'OnUpdateShoot', currentUserAtr);
+	  if(gameInstance!=null)
+		{
+		  gameInstance.SendMessage ('NetworkManager', 'OnUpdateShoot', currentUserAtr);
+		}
+		else if(unityInstance!=null)
+		{
+		  unityInstance.SendMessage ('NetworkManager', 'OnUpdateShoot', currentUserAtr);
+		}
+	 
 	});	
 	
    socket.on('UPDATE_PLAYER_DAMAGE', function(id,health) {
 				      		
 	  var currentUserAtr = id+':'+health;
-	  gameInstance.SendMessage ('NetworkManager', 'OnUpdatePlayerDamage', currentUserAtr);
+	  if(gameInstance!=null)
+		{
+		 gameInstance.SendMessage ('NetworkManager', 'OnUpdatePlayerDamage', currentUserAtr);
+		}
+		else if(unityInstance!=null)
+		{
+		 unityInstance.SendMessage ('NetworkManager', 'OnUpdatePlayerDamage', currentUserAtr);
+		}
+	  
 	});	
 
  socket.on('GAME_OVER', function(target_id,shooter_id,shooter_kills) {
 				      		
 	  var currentUserAtr = target_id+":"+shooter_id+":"+shooter_kills;
-	  gameInstance.SendMessage ('NetworkManager', 'OnGameOver', currentUserAtr);
+	  if(gameInstance!=null)
+		{
+		 gameInstance.SendMessage ('NetworkManager', 'OnGameOver', currentUserAtr);
+		}
+		else if(unityInstance!=null)
+		{
+		 unityInstance.SendMessage ('NetworkManager', 'OnGameOver', currentUserAtr);
+		}
+	  
 	});	
 	
 
  socket.on('USER_DISCONNECTED', function(id) {
 				      		
 	  var currentUserAtr = id+":"+" ";
-	  gameInstance.SendMessage ('NetworkManager', 'OnUserDisconnected', currentUserAtr);
+	  if(gameInstance!=null)
+		{
+		  gameInstance.SendMessage ('NetworkManager', 'OnUserDisconnected', currentUserAtr);
+		}
+		else if(unityInstance!=null)
+		{
+		  unityInstance.SendMessage ('NetworkManager', 'OnUserDisconnected', currentUserAtr);
+		}
+	 
 	});		
 
 
